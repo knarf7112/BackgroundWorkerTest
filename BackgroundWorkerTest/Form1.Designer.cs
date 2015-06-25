935c38ecc36bf73b37922f476bdcb4e534db38fd
knarf7112 <knarf7112@gmail.com>
2015-06-25 09:40:18 +0800

    "#0000: 測試專案建立"

---
+++ BackgroundWorkerTest/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace BackgroundWorkerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartAsync = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.btnCancelAsync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartAsync
+            // 
+            this.btnStartAsync.Location = new System.Drawing.Point(80, 152);
+            this.btnStartAsync.Name = "btnStartAsync";
+            this.btnStartAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnStartAsync.TabIndex = 0;
+            this.btnStartAsync.Text = "Start";
+            this.btnStartAsync.UseVisualStyleBackColor = true;
+            this.btnStartAsync.Click += new System.EventHandler(this.btnStartAsync_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(92, 49);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(53, 12);
+            this.lbResult.TabIndex = 1;
+            this.lbResult.Text = "顯示進度";
+            // 
+            // btnCancelAsync
+            // 
+            this.btnCancelAsync.Location = new System.Drawing.Point(261, 152);
+            this.btnCancelAsync.Name = "btnCancelAsync";
+            this.btnCancelAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelAsync.TabIndex = 2;
+            this.btnCancelAsync.Text = "Cancel";
+            this.btnCancelAsync.UseVisualStyleBackColor = true;
+            this.btnCancelAsync.Click += new System.EventHandler(this.btnCancelAsync_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 222);
+            this.Controls.Add(this.btnCancelAsync);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btnStartAsync);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartAsync;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Button btnCancelAsync;
+    }
+}
+
---
+++ BackgroundWorkerTest/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerTest
+{
+    public partial class Form1 : Form
+    {
+        BackgroundWorker bg1;
+        public Form1()
+        {
+            InitializeComponent();
+            bg1 = new BackgroundWorker();
+            bg1.WorkerReportsProgress = true;
+            bg1.WorkerSupportsCancellation = true;
+            bg1.DoWork += bg1_DoWork;
+            bg1.ProgressChanged += bg1_ProgressChanged;
+            bg1.RunWorkerCompleted += bg1_RunWorkerCompleted;
+        }
+
+        void bg1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                lbResult.Text = "取消!";
+            }
+            else if (e.Error != null)
+            {
+                lbResult.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                lbResult.Text = "完成!";
+            }
+        }
+
+        // This event handler updates the progress. 回報的進度事件
+        void bg1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.lbResult.Text = (e.ProgressPercentage.ToString() + "%") + ":" + e.UserState.ToString();
+        }
+
+        void bg1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            for (int i = 1; i <= 10; i++)
+            {
+                //若外部要求BackgroundWorker中止正在進行的程序執行CancelAsync方法則BackgroundWorker的CancellationPending屬性變為True
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(3000);
+                    //計算一個目前進度值並傳遞給ProgressChanged事件
+                    worker.ReportProgress(i * 10, i);//引發ProgressChanged事件並傳入參數
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartAsync_Click(object sender, EventArgs e)
+        {
+            if (!bg1.IsBusy)
+            {
+                // Start the asynchronous operation.
+                bg1.RunWorkerAsync();
+            }
+        }
+
+        private void btnCancelAsync_Click(object sender, EventArgs e)
+        {
+            if (bg1.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asynchronous operation.
+                bg1.CancelAsync();
+            }
+        }
+    }
+}
